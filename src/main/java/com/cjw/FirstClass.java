b635aea90ff998afbdf1e1b4cd40681a480b8526
dragoncjw2000 <dragoncjw2000@126.com>
2026-04-12 18:09:58 +0800

    change FirstClass 003

--- src/main/java/com/cjw/FirstClass.java
+++ src/main/java/com/cjw/FirstClass.java
@@ -2,6 +2,6 @@ package com.cjw;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("这是我的第一个Java程序！-002");
+        System.out.println("这是我的第一个Java程序！-003");
     }
 }
