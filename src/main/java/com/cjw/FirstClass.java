b395c77429cdd394ab145eba05451c417a30251c
dragoncjw2000 <dragoncjw2000@126.com>
2026-04-13 23:03:52 +0800

    update FirstClass to adjust output statements and improve message formatting

--- src/main/java/com/cjw/FirstClass.java
+++ src/main/java/com/cjw/FirstClass.java
@@ -2,11 +2,13 @@ package com.cjw;
 
 public class FirstClass {
     public static void main(String[] args) {
+        System.out.println("Hello FirstClass");
+        
         SecondClassV2 secondClass = new SecondClassV2();
         String result = secondClass.getSecondString("From First Class");
         System.out.println("这是我的第一个Java程序！-003: " + result);
         System.out.println("这是我的第一个Java程序！-002");
 
-        System.out.println("Hello End: v3");
+        System.out.println("Hello End! V2");
     }
 }
