601a44f46a482b03d467b52ee3139197f6567077
dragoncjw2000 <dragoncjw2000@126.com>
2026-04-13 22:23:43 +0800

    update FirstClass to add additional output statements

--- src/main/java/com/cjw/FirstClass.java
+++ src/main/java/com/cjw/FirstClass.java
@@ -5,5 +5,8 @@ public class FirstClass {
         SecondClassV2 secondClass = new SecondClassV2();
         String result = secondClass.getSecondString("From First Class");
         System.out.println("这是我的第一个Java程序！-003: " + result);
+        System.out.println("这是我的第一个Java程序！-002");
+
+        System.out.println("Hello End");
     }
 }
